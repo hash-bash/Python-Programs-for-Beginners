3c9d5056674f5efbaf11cc04ecf315e23010dbbf
Saurabh Dome <domesaurabh@gmail.com>
2021-04-14 13:12:18 +0530

    Update Sum_of_Divisors.py

--- Sum_of_Divisors.py
+++ Sum_of_Divisors.py
@@ -10,6 +10,6 @@ def sum_divisors(n):
 
 
 print(sum_divisors(0))
-print(sum_divisors(3))  # Should sum of 1
-print(sum_divisors(36))  # Should sum of 1+2+3+4+6+9+12+18
+print(sum_divisors(3))  # Should be sum of 1
+print(sum_divisors(36))  # Should be sum of 1+2+3+4+6+9+12+18
 print(sum_divisors(102))  # Should be sum of 2+3+6+17+34+51
